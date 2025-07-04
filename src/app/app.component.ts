1b981ca30b573645e359943c099e42113c40a1f1
nkastratovic <nkastratovic@gmail.com>
2025-07-04 08:56:42 +0200

    86. Using Signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { InvestmentResultComponent } from './investment-result/investment-result.component';
 import { InvestmentInput } from './user-input/investment-input.model';
@@ -11,14 +11,17 @@ import { UserInputComponent } from './user-input/user-input.component';
   imports: [HeaderComponent, UserInputComponent, InvestmentResultComponent],
 })
 export class AppComponent {
-  resultsData?: {
-    year: number;
-    interest: number;
-    valueEndOfYear: number;
-    annualInvestment: number;
-    totalInterest: number;
-    totalAmountInvested: number;
-  }[];
+  resultsData = signal<
+    | {
+        year: number;
+        interest: number;
+        valueEndOfYear: number;
+        annualInvestment: number;
+        totalInterest: number;
+        totalAmountInvested: number;
+      }[]
+    | undefined
+  >(undefined);
 
   onCalculateInvestmentResults(data: InvestmentInput) {
     const { initialInvestment, annualInvestment, expectedReturn, duration } =
@@ -42,6 +45,6 @@ export class AppComponent {
       });
     }
 
-    this.resultsData = annualData;
+    this.resultsData.set(annualData);
   }
 }
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InvestmentInput } from './investment-input.model';
 
@@ -10,18 +10,23 @@ import { InvestmentInput } from './investment-input.model';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-  @Output() calculate = new EventEmitter<InvestmentInput>();
-  enterdInitialInvestment: number = 0;
-  enterdAnnuagInvestment: number = 0;
-  enterdExpectedReturn: number = 5;
-  enterdDuration: number = 10;
+  calculate = output<InvestmentInput>();
+  enterdInitialInvestment = signal('0');
+  enterdAnnuagInvestment = signal('0');
+  enterdExpectedReturn = signal('5');
+  enterdDuration = signal('10');
 
   onSubmit() {
     this.calculate.emit({
-      initialInvestment: +this.enterdInitialInvestment,
-      annualInvestment: +this.enterdAnnuagInvestment,
-      expectedReturn: +this.enterdExpectedReturn,
-      duration: +this.enterdDuration,
+      initialInvestment: +this.enterdInitialInvestment(),
+      annualInvestment: +this.enterdAnnuagInvestment(),
+      expectedReturn: +this.enterdExpectedReturn(),
+      duration: +this.enterdDuration(),
     });
+
+    this.enterdInitialInvestment.set('0');
+    this.enterdAnnuagInvestment.set('0');
+    this.enterdExpectedReturn.set('5');
+    this.enterdDuration.set('10');
   }
 }
